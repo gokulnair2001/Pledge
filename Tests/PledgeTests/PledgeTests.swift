32ec43136a3dd044f56d1f954a31ba1051988247
Gokul Nair <gokul.getpowerplay.in>
2025-03-29 14:00:35 +0530

    Test case Phase -1

--- Tests/PledgeTests/PledgeTests.swift
+++ Tests/PledgeTests/PledgeTests.swift
@@ -6,30 +6,112 @@
 //
 
 import XCTest
+@testable import Pledge
 
 final class PledgeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    // MARK: - Basic Observable Tests
+    func testBasicObservableSubscription() {
+        let observable = PLObservable<Int>(5)
+        var receivedValue = 0 {
+            didSet {
+                print("Value set: \(receivedValue)")
+            }
         }
+        
+        let initialExpectation = XCTestExpectation(description: "Observer should be notified with initial value")
+        let updatedExpectation = XCTestExpectation(description: "Observer should be notified with updated value")
+        
+        _ = observable.subscribe { value in
+            DispatchQueue.main.async {
+                receivedValue = value
+                if value == 5 {
+                    initialExpectation.fulfill()
+                } else if value == 10 {
+                    updatedExpectation.fulfill()
+                }
+            }
+        }
+        
+        wait(for: [initialExpectation], timeout: 1.0)
+        XCTAssertEqual(receivedValue, 5, "Observer should receive initial value")
+        
+        observable.setValue(10)
+        wait(for: [updatedExpectation], timeout: 1.0)
+        XCTAssertEqual(receivedValue, 10, "Observer should receive updated value")
+    }
+
+    func testMultipleSubscribers() {
+        let observable = PLObservable<String>("initial")
+        var receivedUpdates: [String] = []
+        
+        let initialExpectation = XCTestExpectation(description: "All subscribers should receive the initial value")
+        let updatedExpectation = XCTestExpectation(description: "All subscribers should receive the updated value")
+        
+        for _ in 1...5 {
+            _ = observable.subscribe { value in
+                DispatchQueue.main.async {
+                    receivedUpdates.append(value)
+                    
+                    if receivedUpdates.filter({ $0 == "initial" }).count == 5 {
+                        initialExpectation.fulfill()
+                    } else if receivedUpdates.filter({ $0 == "updated" }).count == 5 {
+                        updatedExpectation.fulfill()
+                    }
+                }
+            }
+        }
+        
+        wait(for: [initialExpectation], timeout: 1.0)
+        XCTAssertEqual(receivedUpdates.count, 5)
+        XCTAssertEqual(receivedUpdates, Array(repeating: "initial", count: 5))
+        
+        receivedUpdates.removeAll()
+        
+        observable.setValue("updated")
+        
+        wait(for: [updatedExpectation], timeout: 1.0)
+        XCTAssertEqual(receivedUpdates.count, 5)
+        XCTAssertEqual(receivedUpdates, Array(repeating: "updated", count: 5))
+    }
+
+    
+    func testUnsubscribe() {
+        let observable = PLObservable<Int>(0)
+        var count1 = 0
+        var count2 = 0
+        
+        let expectation1 = XCTestExpectation(description: "First observer should receive initial value")
+        let expectation2 = XCTestExpectation(description: "Second observer should receive initial value")
+        let updateExpectation = XCTestExpectation(description: "Only the second observer should receive update")
+        
+        let subscription1 = observable.subscribe { _ in
+            count1 += 1
+            if count1 == 1 { expectation1.fulfill() }
+        }
+        
+        observable.subscribe { _ in
+            count2 += 1
+            if count2 == 1 { expectation2.fulfill() }
+            else if count2 == 2 { updateExpectation.fulfill() }
+        }
+        
+        wait(for: [expectation1, expectation2], timeout: 1.0)
+        
+        XCTAssertEqual(count1, 1, "First observer should have received the initial value")
+        XCTAssertEqual(count2, 1, "Second observer should have received the initial value")
+        
+        // Unsubscribe first observer
+        observable.unsubscribe(subscription1)
+        
+        // Update the value
+        observable.setValue(1)
+        
+        wait(for: [updateExpectation], timeout: 1.0) 
+        
+        XCTAssertEqual(count1, 1, "Unsubscribed observer should not receive updates")
+        XCTAssertEqual(count2, 2, "Subscribed observer should continue receiving updates")
     }
 
 }
